aa53310981ead35f51dd2fe292ca863157eef1a3
Jamie <jamiekennedy97@gmail.com>
2023-02-10 09:53:32 +0000

    Update homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 function App() {
-    return <div></div>;
+    return (
+        <div className="min-h-screen w-full bg-slate-900 ">
+            <section className="align-items-center m-auto flex h-48 max-w-screen-2xl flex-col justify-evenly py-10 ">
+                <h1 className="text-center text-4xl text-sky-100">
+                    Frontend Mentor Solution Repository
+                </h1>
+                <div className="m-auto h-1 w-32 rounded-sm bg-sky-200"></div>
+            </section>
+            <section className="h-screen w-full "></section>
+        </div>
+    );
 }
 
 export default App;
